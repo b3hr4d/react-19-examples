99d07093496b0d59825ecbe1d47c138d262b6792
b3hr4d <behradmusiclove@yahoo.com>
2024-05-17 12:50:12 +0300

    chore: Update React and React DOM versions, and fix formatting in App.jsx and FormState.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
-import Users from "./examples/Users";
-import { Suspense } from "react";
-import Theme from './examples/Theme';
-import Action from './examples/Action';
-import Optimistic from "./examples/Optimistic";
-import FormStatus from "./examples/FormStatus";
-import FormState from "./examples/FormState";
+import "./App.css"
+import Users from "./examples/Users"
+import { Suspense } from "react"
+import Theme from "./examples/Theme"
+import Action from "./examples/Action"
+import Optimistic from "./examples/Optimistic"
+import FormStatus from "./examples/FormStatus"
+import FormState from "./examples/FormState"
 
 function App() {
   return (
@@ -16,16 +16,16 @@ function App() {
             <h1 className="text-2xl text-center font-bold mt-5">Loading...</h1>
           }
         >
-          {/* <Action />
+          <Action />
           <FormState />
-         <FormStatus />
-           <Optimistic /> */}
-          {/* <Users /> */}
+          <FormStatus />
+          <Optimistic />
+          <Users />
           <Theme />
         </Suspense>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
